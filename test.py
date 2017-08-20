214b883a39e3454766bf7ad8ee4e89cd1b01d0dd
chubaezeks <ezekschuba@gmail.com>
2017-08-20 21:29:19 +0100

    Learning how to create a spreadsheet with python

--- test.py
+++ test.py
@@ -3,3 +3,31 @@ import my_functions
 
 # Foo call
 my_functions.foo()
+
+
+# rather than add a namespace everytime, we can simply import the library into
+# the local namespace
+from my_functions import *
+
+#this no longer needs the namespace everytime, so...
+foo()
+
+# Creating a new excel sheet in python
+from openpyxl import Workbook
+import random
+
+# Create an excel book
+wb = Workbook()
+
+# Grab the active worksheet
+ws = wb.active
+
+# Data can be assigned directly to cells
+ws['A1'] = "This is a test"
+
+# Rows can be appended
+for i in range(200):
+    ws.append([random.randrange(1000)])
+
+# Save the file
+wb.save("sample.xlsx")
