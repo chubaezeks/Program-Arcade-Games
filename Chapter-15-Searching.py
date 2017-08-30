1ee01de3c0e3d857b5f1f84527ccfc45bbb9877f
chubaezeks <ezekschuba@gmail.com>
2017-08-30 21:33:03 +0100

    Reading into an array

--- Chapter-15-Searching.py
+++ Chapter-15-Searching.py
@@ -7,3 +7,27 @@ for line in file:
     print(line)
 
 file.close()
+
+
+# Reading into an array
+# Read in a file from disk and put it in an array.
+file = open("super_villians.txt")
+
+name_list = []
+for line in file:
+    line = line.strip()
+    name_list.append(line)
+
+file.close()
+
+# To verify that the file was read into the array
+print ( "There were", len(name_list)), "names in the file."
+
+# An alternative
+for name in name_list:
+    print(name)
+
+
+
+
+#Linear Search
