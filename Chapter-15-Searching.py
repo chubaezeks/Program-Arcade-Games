f8ab2729a5c41cb03c1e04088ae53ecf7fdd8986
chubaezeks <ezekschuba@gmail.com>
2017-08-30 22:47:58 +0100

    Create another list waith all items matching a property

--- Chapter-15-Searching.py
+++ Chapter-15-Searching.py
@@ -61,4 +61,49 @@ def has_property(my_alien):
     if my_alien.color.upper() = "GREEN":
         return True
     else:
-        return False     
+        return False
+
+
+# Does at least one item have a property?
+#check if list has an item that has a property - while loop
+def check_if_one_item_has_property(my_list):
+    """Return true if at least one item has a property."""
+    i = 0
+    while i < len(my_list) and not has_property(my_list[i]):
+        i += 1
+
+    if i < len(my_list):
+        # Found an item with the property
+        return True
+    else:
+        # There is no item with the property
+        return False
+
+# The alternative way to do this is through a for loop
+
+def check_if_one_item_has_property_v2:
+    """ Return True if at least one item has a property.
+    Works the same as V1, but less code. """
+    for item in my_list:
+        if has_property(item):
+            return True
+    return False
+
+# Check if all items have a property
+def check_if_all_items_have_property(my_list):
+    """ Return True if ALL items have a property. """
+    for item in my_list:
+        if not has_property(item):
+            return False
+        return True
+
+
+
+# Create another list waith all items matching a property
+def get_matching_items(list):
+    """Build a brand new list that holds all the items that match our property."""
+    matching_list = []
+    for items in list:
+        if has_property(item):
+            matching_list.append(item)
+    return matching_list
